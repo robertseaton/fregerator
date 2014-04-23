37525f49893555870bc7a5dc6b59d9e4c237014f
robert seaton <robb@adbge.org>
2014-04-22 20:51:59 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+> It is unworthy of excellent men to lose hours like slaves in the labour of calculations which could be safely relegated to anyone else if machines were used.
+-- Gottfried Wilhelm Leibniz, 1671
+
+> Calculation becomes deduction.
+-- Gottlob Frege
+
+`fregerator` is an automated theorem prover in Haskell. It extends Frege's "Calculation becomes deduction." Deduction becomes computation, just like [everything else](http://arxiv.org/abs/1102.1612). 
---
+++ fregerator.hs
@@ -0,0 +1,21 @@
+{-# OPTIONS_GHC -Wall #-}
+
+import System.Environment
+
+main :: IO ()
+main = do
+  args <- getArgs
+  case args of
+    ["--theorem", p] -> putStrLn p
+    ["--help"] -> usage
+    _ -> usage
+
+usage :: IO ()
+usage = do
+  self <- getProgName
+  putStr . unlines $
+        concat ["Usage: ", self, " [OPTION] <theorem>"] :
+        "Options:" :
+        " --help Print this message" :
+        " --theorem Attempt to solve a theorem" :
+        []
