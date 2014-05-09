f9983c55ca40277fb467ea368b4ed54436fcbf42
robert seaton <robbpseaton@gmail.com>
2014-05-08 22:24:49 -0500

    First stab at a show function for term.

--- src/fol.hs
+++ src/fol.hs
@@ -1,3 +1,7 @@
+import Data.List(intersperse)
+-- A term is either a function applied to a list of variables, or a variable.
+-- So, the equivalent of Constr String [Term] in more habitual notation is f(a, b, c)
+-- while the equivalent of Var string is 
 data Term = Const String [Term]
             | Var String
 
@@ -10,3 +14,13 @@ data FOL = Impl FOL FOL
          | And FOL FOL
          | Exists String FOL
          | Forall String FOL
+
+instance Show Term where
+  show (Const f args) = show f ++ "(" ++ showargs args ++ ")"
+  show (Var c) = show c
+
+showargs :: [Term] -> String
+showargs a = concat $ intersperse "," (map show a )
+--instance Show FOL where
+  
+
